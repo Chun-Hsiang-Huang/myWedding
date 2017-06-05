52dfe69e1261cd3dd416cb40b742ba2cad449a37
Sam <spadec35823@gmail.com>
2017-06-05 16:20:09 +0800

    改了CDKey

---
+++ App_Code/CDKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// CDKey 的摘要描述
+/// </summary>
+public class CDKey
+{
+    public readonly static string Count = "Count";
+}
